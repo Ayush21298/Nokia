4599ad5adaf0b1425b742ce4d7fdf5af252c9ada
Ayush21298 <patel.ayush08@gmail.com>
2018-09-11 20:09:06 +0530

    Commenting and Refining

--- Alarm/script.py
+++ Alarm/script.py
@@ -1,10 +1,16 @@
 import csv
+import sys
 
-filename = "./Alarm history/BSC_Alarm_history1.txt"
+# Get File Name
+try:
+	filename = sys.argv[1]
+except:
+	filename = "./Alarm history/BSC_Alarm_history1.txt"
+
+# Open File
 files = open(filename,"r")
 
-## x = 0
-
+# Fields of Entry
 instance = []
 date = []
 alarm_type = []
@@ -12,27 +18,23 @@ ne = []
 alarm_no = []
 desc = []
 supp_info = []
-cancel_time = []
 star_rating = []
 new_type = []
 
 rows = []
-
 lines = []
 
+# Reading File
 for line in files:
 	if line.strip()	== "":
 		rows += [lines]
 		lines = []
 	else:
 		lines += [line]
-##	if (x==0):
-##		break
 
 i = 0
 
-## print rows[i][0].strip().split()
-
+# Extracting Fields
 while i < (len(rows)):
 	try:
 		cur_instance = [ int(rows[i][2].strip().split()[0][1:-1]) ]
@@ -45,7 +47,6 @@ while i < (len(rows)):
 		cur_alarm_no = [ rows[i][2].strip().split()[1] ]
 		cur_desc = [ rows[i][2].strip().split()[2] + " " + rows[i][2].strip().split()[3] + " " + rows[i][2].strip().split()[4] ]
 		cur_supp_info = [ rows[i][3].strip().split()[0] + " " + rows[i][3].strip().split()[1] + " " + rows[i][3].strip().split()[2] ]
-		# cur_cancle_time = 
 		if( (rows[i][1].strip().split()[0][0] == '*') or (rows[i][1].strip().split()[0][0] == '.') ):
 			cur_star_rating = [ rows[i][1].strip().split()[0] ]
 		else:
@@ -61,28 +62,16 @@ while i < (len(rows)):
 		alarm_no += cur_alarm_no
 		desc += cur_desc
 		supp_info += cur_supp_info
-		# cancel_time += cur_cancle_time
 		star_rating += cur_star_rating
 		new_type += cur_new_type
 		i += 1
 	except:
 		rows = rows[:i]+rows[i+1:]
 
+# Dictionary
 records = {}
 
-## print new_type
-
-##print len(instance)
-##print len(date)
-##print len(alarm_type)
-##print len(ne)
-##print len(alarm_no)
-##print len(desc)
-##print len(supp_info)
-####print len(cancel_time)
-##print len(star_rating)
-##print len(new_type)
-
+# Updating Dictionary
 for i in range(len(alarm_no)):
 	records[instance[i]] = {}
 	records[instance[i]]['alarm_no'] = alarm_no[i]
@@ -94,17 +83,19 @@ for i in range(len(alarm_no)):
 	##records[instance[i]]['start'] = ""
 	##records[instance[i]]['end'] = ""
 
+# Matching keys as Start / End of Alarm
 for i in range(len(alarm_no)):
 	if(new_type[i] == 'ALARM'):
 		records[instance[i]]['start'] = date[i]
 	elif(new_type[i] == 'CANCEL'):
 		records[instance[i]]['end'] = date[i]
 
-##print new_type
 ##print records
 
+# Headers
 s = "Instance, Start Time, Type, NE, Alarm No, Description, Supp Info, Cancle Time, Star Rating"
 
+# Making CSV
 for key, value in records.iteritems():
 	try:
 		temp = "\n" + str(key) + "," + str(value['start']) + "," + str(value['alarm_type']) + ","  + str(value['ne']) + ","  + str(value['alarm_no']) + ","  + str(value['desc']) + ","  + str(value['supp_info']) + ","  + str(value['end']) + ","  + str(value['star_rating'])
@@ -113,6 +104,3 @@ for key, value in records.iteritems():
 		continue
 
 print s
-
-## print instance
-## print date
